4a27dd88c00e2ddc3697ce5b96ff8069cf3d5bcb
epsilon <martin.olivier1997@gmail.com>
2023-07-05 23:00:31 +0200

    added rand_complex implem

--- src/math_utils.c
+++ src/math_utils.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 #include <math.h>
+#include <complex.h>
 
 float map(double var1, double min1, double max1, double min2, double max2){
 	return min2+(max2-min2)*((var1-min1)/(max1-min1));
 }
 
-float float_rand( float min, float max ){
+float float_rand(float min, float max){
 	float scale = rand() / (float) RAND_MAX; /* [0, 1.0] */
 	return min + scale * ( max - min );      /* [min, max] */
+}
+
+complex rand_complex(complex min, complex max){
+	return (complex) {float_rand(creal(min), creal(max)), float_rand(cimag(min), cimag(max))} ;
 }
