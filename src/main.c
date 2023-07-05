9b54925b9d5fe465e857f0afb19ce48ad2cac091
epsilon <martin.olivier1997@gmail.com>
2023-07-05 14:42:12 +0200

    added screen blacnking in main

--- src/main.c
+++ src/main.c
@@ -17,10 +17,37 @@ int main(){
 	t_params parameters; 
 	parameters.resx = 4960;
 	parameters.resx = 7106;
-	parameters.npoints = 1000;
+	parameters.n_points = 1000;
 	parameters.maxit = 1e6;
 	parameters.n_kernels = 32;
 
+	Display *dpy;
+	Window root;
+	XWindowAttributes wa;
+	GC g;
+
+	XColor blackx, blacks;
+	XColor whitex, whites;
+
+	XAllocNamedColor(dpy, DefaultColormapOfScreen(DefaultScreenOfDisplay(dpy)), "black", &blacks, &blackx);
+	XAllocNamedColor(dpy, DefaultColormapOfScreen(DefaultScreenOfDisplay(dpy)), "white", &whites, &whitex);
+
+	/* open the display (connect to the X server) */
+	dpy = XOpenDisplay (getenv ("DISPLAY"));
+
+	/* get the root window */
+	root = DefaultRootWindow (dpy);
+
+	/* create a GC for drawing in the window */
+	g = XCreateGC (dpy, root, 0, NULL);
+
+	XGetWindowAttributes(dpy, root, &wa);
+
+	XAllocNamedColor(dpy, DefaultColormapOfScreen(DefaultScreenOfDisplay(dpy)), "black", &blacks, &blackx);
+	XAllocNamedColor(dpy, DefaultColormapOfScreen(DefaultScreenOfDisplay(dpy)), "white", &whites, &whitex);
+
+	XSetForeground(dpy, g, blacks.pixel);
+	XFillRectangle (dpy, root, g, 0, 0, wa.width, wa.height);
 
 	srand(time(NULL));
 
