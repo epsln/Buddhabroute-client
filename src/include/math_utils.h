d570faaa599e26994d65342508c3232902ecd44b
epsilon <martin.olivier1997@gmail.com>
2023-07-05 14:13:15 +0200

    include complex in math utils

--- src/include/math_utils.h
+++ src/include/math_utils.h
@@ -1,5 +1,8 @@
 #ifndef math_utils
 #define math_utils
+
+#include <complex.h>
+
 float map(double var1, double min1, double max1, double min2, double max2);
 
 float float_rand(float min, float max );
