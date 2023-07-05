d8598b0cb3fc938e8238dbf2c054b409d5c3c100
epsilon <martin.olivier1997@gmail.com>
2023-07-05 14:11:39 +0200

    added proto for rand complex

--- src/include/math_utils.h
+++ src/include/math_utils.h
@@ -2,7 +2,8 @@
 #define math_utils
 float map(double var1, double min1, double max1, double min2, double max2);
 
-float float_rand( float min, float max );
+float float_rand(float min, float max );
+float rand_complex(complex min, complex max );
 
 int progressBar(int i, int iMax, int oldMark);
 
