0855f75bbf425ecad92e075db85b80f7164c72c3
Martin Olivier <martin.olivier@linkfluence.com>
2023-07-17 17:24:43 +0200

    removed read checkpoint because slow, changed write to use checkpoint name of params

--- src/readFiles.c
+++ src/readFiles.c
@@ -7,33 +7,16 @@
 
 const char* getfield(char* line, int num){
 	const char* tok;
-	for (tok = strtok(line, ",");
-			tok && *tok;
-			tok = strtok(NULL, ",\n")){
+	for (tok = strtok(line, ","); tok && *tok; tok = strtok(NULL, ",\n")){
 		if (!--num)
 			return tok;
 	}
+
 	return NULL;
 }
 
-void readCheckpoint(params_t *params, u_int32_t* histogram){
-	char line[8192];
-	FILE* stream = fopen("checkpoint.txt", "r");
-	if (stream == NULL) return;
-
-	for (int i = 0; i < params->resx; i++){
-		fgets(line, 8192, stream);
-		for (int j = 0; j < params->resy; j++){
-			char* tmp = strdup(line);
-			histogram[i * params->resy + j] = atoi(getfield(tmp, j));
-			free(tmp);
-		}
-	}
-	fclose(stream);
-}
-
 void writeCheckpoint(params_t* params, u_int32_t* histogram){
-	FILE* stream = fopen("checkpoint.txt", "w+");
+	FILE* stream = fopen(params->checkpoint_filename, "w+");
 	for (int i = 0; i < params->resx; i++){
 		for (int j = 0; j < params->resy; j++){
 			fprintf(stream, "%u,",histogram[i * params->resy + j] );
