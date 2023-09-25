457959a31ff08dc4eeac39f2ac715fe7e7fb268c
epsilon <martin.olivier1997@gmail.com>
2023-09-25 16:56:07 +0200

    upgrade export function to do it all

--- export.py
+++ export.py
@@ -52,61 +52,34 @@ if __name__ == '__main__':
     data['maxiter'] = config['IMAGE']['maxiter']
     data['shape'] = (config['IMAGE']['resx'], config['IMAGE']['resy'])
     data['nickname'] = config['EXPORT']['nickname']
-    logger.debug(f'{data}')
-
-    proc = subprocess.Popen(['python','fake_utility.py'],stdout=subprocess.PIPE)
-    histogram = np.zeros((int(config['IMAGE']['resx']), int(config['IMAGE']['resy'])))
-    while True:
-        output = process.stdout.readline()
-        histogram = np.array([float(x) for x in output.split(' ')])
-        print(histogram, end='', flush=True)
-
-    
-#    full_input_path = expanduser(config['EXPORT']['input_dir'])
-#    filename_list = [join(full_input_path, f) for f in listdir(full_input_path)
-#                    if isfile(join(full_input_path, f)) and f.endswith(".csv")
-#                    and f != "checkpoint_buffer.csv"]
-#    logger.debug(f'{filename_list}')
-#
-#    histogram = np.zeros((int(config['IMAGE']['resx']), int(config['IMAGE']['resy'])))
-#
-#    for input_file in filename_list:
-#        try:
-#            histo_df = pd.read_csv(input_file)
-#            logger.debug(f'reading {input_file}')
-#        except ParserError:
-#            logger.info(f'{input_file} failed to parse, removing')
-#            remove(input_file)
-#            continue
-#        except UnicodeDecodeError:
-#            logger.info(f'{input_file} failed to decode, removing')
-#            remove(input_file)
-#            continue
-#        except EmptyDataError:
-#            logger.info(f'{input_file} is empty, removing')
-#            remove(input_file)
-#            continue
-#
-#        if histo_df.shape != (int(config['IMAGE']['resx']), int(config['IMAGE']['resy'])):
-#            logger.info(f'{input_file} is not the correct dimension, removing')
-#            remove(input_file)
-#            continue
-#
-#        histogram = np.add(histogram, histo_df.values)
-#        remove(input_file)
-#
+    data['version'] = config['EXPORT']['version']
     url = f"{config['EXPORT']['url']}:{config['EXPORT']['port']}{config['EXPORT']['route']}"
-    logger.debug(f'{url}')
-
     headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-    logger.debug(f'{headers}')
-    # with compression to save bandwidth
-    data['histogram'] = base64.b64encode(
-        zlib.compress(
-            histogram.tobytes()
-        )
-    ).decode('utf-8')
+    logger.debug(f'Sending {data} to {url}')
 
-    import pdb
-    pdb.set_trace()
-    r = requests.post(url, json=data, headers=headers)
+    histogram = np.zeros((int(config['IMAGE']['resx']), int(config['IMAGE']['resy'])))
+    process_list = []
+
+    for n in range(int(config['IMAGE']['workers'])):
+        process_list.append(subprocess.Popen(['./buddhabroute-dev'],stdout=subprocess.PIPE))
+
+    while True:
+        for n, process in enumerate(process_list): 
+            output = process.stdout.readline().decode("ascii")
+            histogram = [float(x) for x in output.replace('\n', '').split(' ')[:-1]]
+            try:
+                histogram = np.reshape(histogram, (int(config['IMAGE']['resx']), int(config['IMAGE']['resy'])))
+            except ValueError:
+                logger.error(f"Wrong shape in the histogram ! Check size. Crashing.") 
+                sys.exit()
+            logger.debug(f'proc {n}: {histogram.shape}')
+
+
+            # with compression to save bandwidth
+            data['histogram'] = base64.b64encode(
+                zlib.compress(
+                    histogram.tobytes()
+                )
+            ).decode('utf-8')
+
+            r = requests.post(url, json=data, headers=headers)
