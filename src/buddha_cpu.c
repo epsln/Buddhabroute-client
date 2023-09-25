79bd87a884c69abe7140094be063c24fa74c44f6
epsilon <martin.olivier1997@gmail.com>
2023-09-25 17:30:54 +0200

    changed bounds

--- src/buddha_cpu.c
+++ src/buddha_cpu.c
@@ -44,7 +44,7 @@ void buddhaCPU(params_t* p_params, xStuff_t* x){
 		for(int i = 0; i < p_params->maxiter; i++){
 			if (trajs[i] == -10) break;
 			int x = (int)map(creal(trajs[i]), -0.5, 0.5, 0, p_params->resx);
-			int y = (int)map(cimag(trajs[i]), -0.5 * invSqr2, 0.5 * invSqr2, 0, p_params->resy);
+			int y = (int)map(cimag(trajs[i]), -0.25 * invSqr2, 0.75 * invSqr2, 0, p_params->resy);
 			if (x >= 0 && x < p_params->resx && y >= 0 && y < p_params->resy){
 				histogram[x + y * p_params->resx]++;
 			}
