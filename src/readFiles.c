eac6ef5998ec86e1389f4accc4b61f059571c042
epsilon <martin.olivier1997@gmail.com>
2023-10-08 23:00:17 +0200

    created simple handler

--- src/readFiles.c
+++ src/readFiles.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "include/config.h"
+#include "include/ini.h"
 #include "include/readFiles.h"
 
 const char* getfield(char* line, int num){
@@ -13,6 +14,27 @@ const char* getfield(char* line, int num){
 	}
 
 	return NULL;
+}
+
+static int handler(void* params, const char* section, const char* name,
+                   const char* value){
+    params_t* pconfig = (params_t*)params;
+
+    #define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
+    if (MATCH("IMAGE", "resx")) {
+        pconfig->resx= atoi(value);
+    } else if (MATCH("IMAGE", "resy")) {
+        pconfig->resy = atoi(value);
+    } else if (MATCH("IMAGE", "resy")) {
+        pconfig->maxiter = atoi(value);
+    } else if (MATCH("EXPORT", "n_points")) {
+        pconfig->n_points = atoi(value);
+    } else if (MATCH("EXPORT", "sleep_time")) {
+        pconfig->sleep_time = atoi(value);
+    } else {
+        return 0;  /* unknown section/name, error */
+    }
+    return 1;
 }
 
 void writeCheckpoint(params_t* params, u_int32_t* histogram){
