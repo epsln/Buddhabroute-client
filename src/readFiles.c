570d1e70c7118c39c3d93654a878baba2f5ecc58
epsilon <martin.olivier1997@gmail.com>
2023-10-08 23:08:08 +0200

    removed handler from readfile

--- src/readFiles.c
+++ src/readFiles.c
@@ -16,25 +16,6 @@ const char* getfield(char* line, int num){
 	return NULL;
 }
 
-static int handler(void* params, const char* section, const char* name, const char* value){
-    params_t* pconfig = (params_t*)params;
-
-    #define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
-    if (MATCH("IMAGE", "resx")) {
-        pconfig->resx= atoi(value);
-    } else if (MATCH("IMAGE", "resy")) {
-        pconfig->resy = atoi(value);
-    } else if (MATCH("IMAGE", "resy")) {
-        pconfig->maxiter = atoi(value);
-    } else if (MATCH("EXPORT", "n_points")) {
-        pconfig->n_points = atoi(value);
-    } else if (MATCH("EXPORT", "sleep_time")) {
-        pconfig->sleep_time = atoi(value);
-    } else {
-        return 0;  /* unknown section/name, error */
-    }
-    return 1;
-}
 
 void writeCheckpoint(params_t* params, u_int32_t* histogram){
 	for (int i = 0; i < params->resx; i++){
